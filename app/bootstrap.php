3d882b4fea24c839954582c244edc726062196ba
Jakub Bouček <pan@jakubboucek.cz>
2019-01-16 07:40:24 +0100

    Debug - improve logging with custom mailer

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,13 +4,16 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
+// Debugging
 App\Model\DebugEnabler::setWorkDir(__DIR__ . '/../temp');
-
 $configurator->setDebugMode(App\Model\DebugEnabler::isDebug() ? true : []);
-
 $configurator->enableTracy(__DIR__ . '/../log', 'pan@jakubboucek.cz');
-\Tracy\Debugger::getLogger()->emailSnooze = '1 hour';
+/** @var \Tracy\Logger $logger */
+$logger = \Tracy\Debugger::getLogger();
+$logger->emailSnooze = '1 hour';
+$logger->fromEmail = 'noreply@prazskybarcamp.cz';
 
+// App
 $configurator->setTimeZone('Europe/Prague');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
